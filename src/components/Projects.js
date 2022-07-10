52421948e377d87c0e65476e0d4be19f570e8868
Manthan <87055332+ManthanUgemuge@users.noreply.github.com>
2022-07-10 21:56:19 +0530

    initial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,101 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/LinkdedIn Clone.png";
+import projImg2 from "../assets/img/Eccomerce.png";
+import projImg3 from "../assets/img/PetShop.png";
+import projImg4 from "../assets/img/Tin Dog.png";
+import projImg5 from "../assets/img/Youtube Thumbnail Downloader.png";
+import projImg6 from "../assets/img/TravelIndia.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "LinkdedIn Clone",
+      description: "React Js",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Eccomerce",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "PetShop",
+      description: "PHP & Laravel",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Tin Dog",
+      description: "BootStrap",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Youtube Thumbnail Downloader",
+      description: "Design & Development",
+      imgUrl: projImg5,
+    },
+    {
+      title: "TravelIndia",
+      description: "HTML & CSS",
+      imgUrl: projImg6,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
